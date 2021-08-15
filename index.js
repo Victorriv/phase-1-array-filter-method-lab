f1ba1546b6e117f6a288bfb3b578be0f9716da0b
Victor <victorrivera714@yahoo.com>
2021-08-14 20:50:48 -0700

    commit tree 72318c4312e1ae8e5499ce4b3b3a8e3a6f8c8a06

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+//// Code your solution here
+function findMatching(driversArray, string){
+   
+    return driversArray.filter(name => name.toUpperCase() === string.toUpperCase()) 
+    
+        
+}
+
+function fuzzyMatch(nameArray, letters){
+   /* "hello".slice(0,2) //=> "he" 
+    "Sa".length //=> 2
+    "Sammy".slice(0,2) //=> "Sa"
+    */
+    return nameArray.filter(name => name.slice(0, letters.length) === letters)
+
+
+}
+
+function matchName(nameofArray, nameWanted){
+return nameofArray.filter(save=> save.name === nameWanted)
+
+}
+
